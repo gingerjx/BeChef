fc2d7af4413c9bfe97a6980779bfef2e9d81feb9
Piotr Kalota <piotrkalota99@gmail.com>
2020-12-16 00:34:50 +0100

    Fix large home view

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Untitled</title>
-    <meta name="description" content="This is an example of a meta description.">
+    <title>BeChef</title>
+    <meta name="description" content="BeChef">
     <link rel="stylesheet" type="text/css" href="view/styles/style.css?v=<?php echo time(); ?>">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   </head>
